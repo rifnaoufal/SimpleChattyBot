1d30d5df85283bbe64e03414168ad2357164e05d
NaoufalAndichi <00313992@rijnijssel.nl>
2022-02-17 11:37:21 +0100

    bot finished

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,15 +6,16 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        greet("Aid", "2022");
+        greet("Aid", "2018");
         remindName();
         guessAge();
         count();
+        test();
         end();
     }
 
-    static void greet(String Aid, String birthYear) {
-        System.out.println("Hello! My name is " + Aid + ".");
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
     }
@@ -26,7 +27,7 @@ public class SimpleBot {
 
     static void guessAge() {
         System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
@@ -44,7 +45,16 @@ public class SimpleBot {
 
     static void test() {
         System.out.println("Let's test your programming knowledge.");
-        
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+
+        Scanner scan = new Scanner(System.in);
+        while (scan.nextInt() != 2) {
+            System.out.println("Please, try again.");
+        }
     }
 
     static void end() {
